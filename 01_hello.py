8a85fb44401f062e5cc1ed1312159f13d263bec1
Suraj <iamsurzpandey@gmail.com>
2022-12-02 12:44:46 +0545

    First program in python

---
+++ 01_hello.py
@@ -0,0 +1,12 @@
+import os
+import flask
+print("Hello world") 
+# Single line cmt
+'''Multiple
+line
+cmt '''
+print(''' hello
+its 
+me ''') # for multi line print use single qutation for 3 times
+
+
