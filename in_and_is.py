a1124ec10b1157d5765f30d30326519b9847eb60
Suraj <iamsurzpandey@gmail.com>
2022-12-04 16:13:01 +0545

    is and in in python

---
+++ in_and_is.py
@@ -0,0 +1,11 @@
+# a = None
+# if a is None: # or we can write in () also.
+#     print("yes")
+# else:
+#     print("No")
+
+
+
+b = [45, 46, 47]
+print(45 in b) # return true
+print(466 in b) # return false
