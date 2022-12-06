04be477523f314a0e0c1f25ad6e62e40b807988e
Suraj <iamsurzpandey@gmail.com>
2022-12-06 11:38:58 +0545

    logical and relational operator in python

---
+++ logical_and_relational_operator.py
@@ -0,0 +1,5 @@
+age = int(input("Enter the age of the client: \n"))
+if (age>=18 and age<45): # or we can write "or" and "not" in and also
+    print("You can join us")
+else:
+    print("you cannot join with us")
