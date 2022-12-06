51bda6b5c1ffa0f8983562c45546bd172018e3bc
Suraj <iamsurzpandey@gmail.com>
2022-12-06 11:44:36 +0545

    finding the greatest as well as adding number

---
+++ pratice1.py
@@ -0,0 +1,13 @@
+a = input("Enter the 1st num: ")
+a = int(a)
+print(a)
+b = input("Enter the 2nd num: ")
+b = int(b)
+print(b)
+print("the sum of a and b is:", a + b)
+c = (a>b)
+print("1st number is greater than 2nd one?")
+print(c)
+d = a**2
+# or we can also write a*a 
+print("the square of 1st num is: ", d)
